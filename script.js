03a79678fb38a495851ade6d4ae3dfd298ae3eb4
Christian Torres <christian.torresgaviria@elev.ga.ntig.se>
2023-04-23 15:11:49 +0200

    Jag har gjort om min kod för att jag gjorde et för komplicert och det blev för mcyket så jag gjorde om det mycket simpligare.

--- script.js
+++ script.js
@@ -11,116 +11,72 @@ function toggleCart() {
   backdrop.classList.toggle('is-visible');
 }
 
-for (let i = 0; i < addToCartButtons.length; i++) {
-  addToCartButtons[i].addEventListener('click', () => {
-    const name = addToCartButtons[i].dataset.name;
-    const price = addToCartButtons[i].dataset.price;
+addToCartButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    const name = button.dataset.name;
+    const price = button.dataset.price;
 
-    const cartItems = cartItemContainer ? Array.from(cartItemContainer.children) : [];
-    const existingCartItem = cartItems.find(item => item.dataset.name === name);
+    const cartItem = document.createElement('li');
+    cartItem.classList.add('cart-items');
+    cartItem.dataset.name = name;
+    cartItem.dataset.price = price;
 
-    if (existingCartItem) {
-      const quantityText = existingCartItem.querySelector('.item-information');
-      const quantity = parseInt(quantityText.textContent) + 1;
-      quantityText.textContent = `${quantity}x`;
+    const productDetails = document.createElement('div');
+    productDetails.classList.add('item-information');
 
-      const productPrice = existingCartItem.querySelector('.product-price');
-      const existingPrice = parseInt(productPrice.textContent.replace('$', ''));
-      const newPrice = existingPrice + parseInt(price);
-      productPrice.textContent = `$${newPrice}`;
+    const productText = document.createElement('span');
+    productText.classList.add('text-style-odibee');
 
-      totalPrice += parseInt(price);
-      if (cartAmount) {
-        cartAmount.textContent = `$${totalPrice}`;
-      }
-    } else {
-      const cartItem = document.createElement('li');
-      cartItem.classList.add('cart-items');
-      cartItem.dataset.name = name;
+    const quantityText = document.createElement('span');
+    quantityText.classList.add('item-information', 'text-style-odibee');
+    quantityText.textContent = '1x';
 
-      const productDetails = document.createElement('div');
-      productDetails.classList.add('item-information');
+    const productName = document.createElement('span');
+    productName.textContent = name;
 
-      const productQuantity = document.createElement('span');
-      productQuantity.classList.add('productQuantity', 'text-style-odibee');
+    productText.appendChild(quantityText);
+    productText.appendChild(productName);
 
-      const quantityText = document.createElement('span');
-      quantityText.classList.add('item-information', 'text-style-odibee');
-      quantityText.textContent = '1x';
+    const productPrice = document.createElement('span');
+    productPrice.classList.add('product-price','text-style-odibee');
+    productPrice.textContent = `$${price}`;
 
-      const productName = document.createElement('span');
-      productName.textContent = name;
+    const productRemoveButton = document.createElement('div');
+    productRemoveButton.classList.add('remove-item-button');
 
-      productQuantity.appendChild(quantityText);
-      productQuantity.appendChild(productName);
+    const removeButton = document.createElement('span');
+    removeButton.innerHTML = '&#10005;';
 
-      const productPrice = document.createElement('div');
-      productPrice.classList.add('product-price', 'text-style-odibee');
-      productPrice.textContent = `$${price}`;
+    productRemoveButton.appendChild(removeButton);
 
-      const productRemoveButton = document.createElement('div');
-      productRemoveButton.classList.add('remove-item-button');
+    productDetails.appendChild(productText);
+    productDetails.appendChild(productPrice);
 
-      const removeButton = document.createElement('span');
-      removeButton.innerHTML = '&#10005;';
+    cartItem.appendChild(productDetails);
+    cartItem.appendChild(productRemoveButton);
 
-      productRemoveButton.appendChild(removeButton);
-
-      productDetails.appendChild(productQuantity);
-      productDetails.appendChild(productPrice);
-
-      cartItem.appendChild(productDetails);
-      cartItem.appendChild(productRemoveButton);
-
-      if (cartItemContainer) {
-        cartItemContainer.appendChild(cartItem);
-      }
-
-      totalPrice += parseInt(price);
-      if (cartAmount) {
-        cartAmount.textContent = `$${totalPrice}`;
-      }
-
-      
-      const removeButtonElement = cartItem.querySelector('.remove-item-button');
-      removeButtonElement.addEventListener('click', () => {
-        console.log('Product remove button clicked');
-        cartItem.remove();
-        totalPrice -= parseInt(price);
-        if (cartAmount) {
-          cartAmount.textContent = `$${totalPrice}`;
-        }
-      });
+    if (cartItemContainer) {
+      cartItemContainer.appendChild(cartItem);
     }
+
+    totalPrice += parseInt(price);
+    if (cartAmount) {
+      cartAmount.textContent = `$${totalPrice}`;
+    }
+
+    const removeButtonElement = cartItem.querySelector('.remove-item-button');
+    removeButtonElement.addEventListener('click', () => {
+    console.log('Product remove button clicked');
+    cartItem.remove();
+    totalPrice -= parseInt(price);
+    if (cartAmount){
+      cartAmount.textContent = `$${totalPrice}`;
+    }
+    });
   });
-}
+});
+
 
 checkoutButton.addEventListener('click', () => {
   alert('Purchase complete!');
 });
-
-const removeCartItemButtons = document.getElementsByClassName('remove-item-button');
-for (let i = 0; i < removeCartItemButtons.length; i++) {
-  const button = removeCartItemButtons[i];
-  button.addEventListener('click', (event) => {
-    const cartItem = event.target.parentElement.parentElement;
-    const name = cartItem.dataset.name;
-    const price = parseInt(cartItem.querySelector('.product-price').textContent.replace('$', ''));
-    const quantityElement = cartItem.querySelector('.item-information');
-    let quantity = parseInt(quantityElement.textContent);
-    const cartItems = Array.from(cartItemContainer.children);
-    const existingCartItem = cartItems.find(item => item.dataset.name === name);
-    if (quantity > 1) {
-      quantity--;
-      quantityElement.textContent = `${quantity}x`;
-      existingCartItem.querySelector('.item-information').textContent = `${quantity}x`;
-      existingCartItem.querySelector('.product-price').textContent = `$${quantity * price}`;
-    } else {
-      cartItemContainer.removeChild(cartItem);
-    }
-    totalPrice -= price;
-    if (cartAmount) {
-      cartAmount.textContent = `$${totalPrice}`;
-    }
-  });
-}
